a2b16b889a60fac8bfafdedb6e7be480f7fe93f0
Mr. Marvin Jupiter Roilo Vargas <mvargas2@my.centennialcollege.ca>
2018-11-15 23:12:31 -0500

    GUKO TRUNKS

---
+++ NavMesh/test/test/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle  {
+
+    public GameObject powerUp;
+    public float speed;
+    public Vector2 spawnPosition;
+
+
+
+}
